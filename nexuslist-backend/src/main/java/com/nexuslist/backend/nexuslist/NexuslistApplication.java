501ce42e4344a5e5a124ecc3b45a4099cb729176
Ethan Hanks <ethanjhanks99@gmail.com>
2025-07-19 21:35:38 -0600

    initialized backend

---
+++ nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/NexuslistApplication.java
@@ -0,0 +1,13 @@
+package com.nexuslist.backend.nexuslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuslistApplication.class, args);
+	}
+
+}
---
+++ nexuslist-backend/src/test/java/com/nexuslist/backend/nexuslist/NexuslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexuslist.backend.nexuslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
