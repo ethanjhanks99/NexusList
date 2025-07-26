6bf4cea46b781020d1ddc4b70000ccf15d1e267b
Ethan Hanks <ethanjhanks99@gmail.com>
2025-07-25 20:02:43 -0600

    removed unused libraries

--- nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/entities/Folder.java
+++ nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/entities/Folder.java
@@ -1,12 +1,9 @@
 package com.nexuslist.backend.nexuslist.entities;
 
-import java.util.ArrayList;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
