9d956f8503520c7f1caec30f4ce098d6d3ae237d
Ethan Hanks <ethanjhanks99@gmail.com>
2025-10-01 10:27:17 -0600

    task service update

--- nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/Task/TaskService.java
+++ nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/Task/TaskService.java
@@ -1,8 +1,24 @@
 package com.nexuslist.backend.nexuslist.Task;
 
+import java.util.List;
+import java.time.LocalDateTime;
+
+import com.nexuslist.backend.nexuslist.Tag.Tag;
+
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
     
+    public List<Task> getTasksByCriteria(
+        Priority priority,
+        boolean completed,
+        Tag tag,
+        LocalDateTime dueDate
+    ) {
+
+        return taskRepository.findAll(spec);
+    }
 }
