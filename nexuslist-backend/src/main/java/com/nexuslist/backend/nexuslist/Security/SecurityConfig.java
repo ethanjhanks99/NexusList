3b2c155a02043dc44eb069b913147984232f99ac
Ethan Hanks <ethanjhanks99@gmail.com>
2025-10-01 19:12:18 -0600

    SecurityConfig

--- nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/Security/SecurityConfig.java
+++ nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/Security/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.nexuslist.backend.nexuslist.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Responcibilies
@@ -15,5 +21,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+            http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+            return http.build();
+    }
 }
