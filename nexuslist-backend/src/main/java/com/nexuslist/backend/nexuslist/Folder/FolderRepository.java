9436fc2bcc951fa375d1f640ed29b2f6cb71f2c4
Ethan Hanks <ethanjhanks99@gmail.com>
2025-10-05 21:16:24 -0600

    added functions for Folder

--- nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/Folder/FolderRepository.java
+++ nexuslist-backend/src/main/java/com/nexuslist/backend/nexuslist/Folder/FolderRepository.java
@@ -1,8 +1,16 @@
 package com.nexuslist.backend.nexuslist.Folder;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import com.nexuslist.backend.nexuslist.User.User;
+
 public interface FolderRepository extends JpaRepository<Folder, Long>, JpaSpecificationExecutor<Folder> {
     
+    Optional<Folder> findByName(String name);
+
+    List<Folder> findByUser(User user);
 }
